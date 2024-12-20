f6a8d696403c6c998d0c57128e8a7eb6584e68eb
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-12-20 19:32:34 -0300

    build: setup eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,62 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
-
 export default tseslint.config(
-  { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    ignores: ['**/*.config.js', '**/*.config.cjs', '**/*.config.ts', 'dist'],
+  },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      'plugin:prettier/recommended',
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: require('@typescript-eslint/parser'),
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        sourceType: 'module',
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      react: require('eslint-plugin-react'),
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+      prettier: require('eslint-plugin-prettier'),
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      'prettier/prettier': 'error',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'react/prop-types': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+      'no-console': 'warn',
+      quotes: ['error', 'single'],
+      'react/self-closing-comp': 'warn',
+      'react/no-danger': 'warn',
+      'no-magic-numbers': ['warn', { ignore: [0, 1] }],
+      indent: ['error', 2],
+      'max-len': ['warn', { code: 80 }],
+      'prefer-const': 'warn',
+      semi: ['error', 'always'],
     },
-  }
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
 );
