456caadacf9601aea07105f39a14aadafef33a30
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-12-22 14:59:44 -0300

    refactor: enhance renderWithProviders to include TimerProvider for context management

--- src/test/utils.tsx
+++ src/test/utils.tsx
@@ -1,6 +1,13 @@
 import { render } from '@testing-library/react';
 import { ReactElement } from 'react';
+import { TimerProvider } from '../contexts/TimerContext';
 
 export function renderWithProviders(ui: ReactElement) {
-  return render(ui);
+  const { rerender } = render(
+    <TimerProvider>
+      {ui}
+    </TimerProvider>
+  );
+
+  return { rerender }
 }
